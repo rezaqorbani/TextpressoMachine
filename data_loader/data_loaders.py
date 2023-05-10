17183322223697f13b46a5a05e63d768da945058
Zineb Senane <senane.zineb.mpsi@gmail.com>
2023-05-10 18:36:13 +0200

    add data loader

---
+++ data_loader/data_loaders.py
@@ -0,0 +1,42 @@
+
+from datasets import load_dataset
+
+
+class XSumDataLoader():
+    """
+    XSUM dataloader
+    """
+    def __init__(self, tokenizer, max_input_length, max_target_length, prefix='summarize'):
+      
+        self.MAX_INPUT_LENGTH = max_input_length
+        self.MAX_TARGET_LENGTH = max_target_length
+        self.prefix = prefix
+
+        ## Load data
+        self.datasets = load_dataset("xsum")
+        
+        ## Load pretrained Tokenizer
+        self.tokenizer = tokenizer
+        
+        ## Tokenize data
+        self.tokenized_datasets=self.datasets.map(self.tokenize, batched=True)
+        
+        
+        
+        
+        
+    def tokenize(self, examples):
+        # Tokenize input texts
+        inputs = [self.prefix + doc for doc in examples["document"]]
+        model_inputs = self.tokenizer(inputs, max_length=self.MAX_INPUT_LENGTH, truncation=True)
+
+        # tokenize summaries
+        labels = self.tokenizer(examples["summary"], max_length=self.MAX_TARGET_LENGTH, truncation=True)
+        model_inputs["labels"] = labels["input_ids"]
+
+        return model_inputs
+    
+    
+                
+        
+
